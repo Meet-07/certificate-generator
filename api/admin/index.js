eaa11a70941a033943785ed44f8b550eb4c638c1
Meet <castingstrange@gmail.com>
2026-09-18 13:41:08 +0530

    Add public/admin.html and cleanUrls routing

--- api/admin/index.js
+++
@@ -1,9 +0,0 @@
-const { getAdminSpaHtml } = require('../../server');
-
-module.exports = (req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'text/html; charset=utf-8',
-    'Cache-Control': 'no-cache, no-store, must-revalidate'
-  });
-  return res.end(getAdminSpaHtml());
-};
